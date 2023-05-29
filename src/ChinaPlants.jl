b056185e84c40dfc43f0f6baa46db613a5964be4
Yu-Chang YANG <yang.yc.allium@gmail.com>
2023-05-29 14:54:39 +0800

    feat: add initialization routine

--- src/ChinaPlants.jl
+++ src/ChinaPlants.jl
@@ -2,7 +2,7 @@
 
 module ChinaPlants
 
-export getdbpath
+export getdbpath, gettreepath, initialize
 
 using DataDeps
 using SymSpellChecker
@@ -42,6 +42,63 @@ function gettreepath()
 		@datadep_str name
 	end
 	return joinpath(path, "time_tree_13_663sp.tre")
+end
+
+function simplify!(table, headers)
+	codes = view(table, :, headers["name_code"])
+	acodes = view(table, :, headers["accepted_name_code"])
+	@assert allunique(codes)
+	@assert issubset(acodes, codes)
+	acodeufs = Dict{String, String}()
+	for code = codes
+		acodeufs[code] = code
+	end
+	for (code, acode) = zip(codes, acodes)
+		acodeufs[code] = acode
+	end
+	function findroot(acodeufs, code)
+		acode = acodeufs[code]
+		if acodeufs[acode] != acode
+			return acodeufs[code] = acodeufs[acode] = findroot(acodeufs, acode)
+		end
+		return acode
+	end
+	for i = eachindex(codes)
+		acodes[i] = findroot(acodeufs, codes[i])
+	end
+	return table
+end
+
+function getname2row(table, headers)
+	col(str) = view(table, :, headers[str])
+	nrow = size(table, 1)
+	rels = zeros(nrow)
+	rels[getindex.((code2row,), unique(col("accepted_name_code")))] .+= 100
+	rels[.!ismissing.(col("species_c"))] .+= 10
+	rels[.!startswith.(string.(col("author")), "auct. non")] .+= 1
+	name2row = Dict{String, Int}()
+	for (i, name) = enumerate(col("canonical_name"))
+		(!haskey(name2row, name) || rels[i] > rels[name2row[name]]) && 
+			(name2row[name] = i)
+	end
+	return name2row
+end
+
+function initialize()
+	xlsx = XLSX.readxlsx(getdbpath())
+	sheet = xlsx["scientific_names"][:]
+	global headers = Dict(sheet[1, :] .=> axes(sheet, 2))
+	global table = sheet[2:end, :]
+	col(str) = view(table, :, headers[str])
+	codes = col("name_code")
+	global code2row = Dict(codes .=> eachindex(codes))
+	col("accepted_name_code")[col("name_code") .== "T20171000078632"] .= 
+		"T20211000001316" # special patch for v1.043 (2023)
+	simplify!(table, headers)
+	arows = sort!(getindex.((code2row,), unique(col("accepted_name_code"))))
+	@assert findall(col("name_code") .== col("accepted_name_code")) == arows
+	global name2row = getname2row(table, headers)
+	@assert issubset(arows, values(name2row))
 end
 
 end # module ChinaPlants
